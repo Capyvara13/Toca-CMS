a38b8ebe8a59c68ec74774aa40636f68b70f4b0f
Capyvara13 <tiagorafleite@gmail.com>
2025-09-24 10:52:53 -0300

    Add dependencies

---
+++ src/main/java/com/atdo/toca_cms/TocaCmsApplication.java
@@ -0,0 +1,13 @@
+package com.atdo.toca_cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TocaCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TocaCmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atdo/toca_cms/TocaCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atdo.toca_cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TocaCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
